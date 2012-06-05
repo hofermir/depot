c7f1010ad2601d78199835c9e20b69a22488ec38
Mirko Hofer <hofermir@students.zhaw.ch>
2012-06-05 11:23:35 +0200

    finish kapitel 11

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class CartsController < ApplicationController
   # GET /carts
   # GET /carts.json
@@ -13,18 +21,17 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
-  begin
-    @cart = Cart.find(params[:id])
-	rescue ActiveRecord::RecordNotFound
-  logger.error "Attempt to access invalid cart #{params[:id]}"
-  redirect_to store_url, notice: 'Invalid cart'
-  else
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @cart }
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @cart }
+      end
     end
-	end
   end
 
   # GET /carts/new
@@ -80,11 +87,10 @@ class CartsController < ApplicationController
   def destroy
     @cart = current_cart
     @cart.destroy
-	session[:cart_id] = nil
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to store_url,
-      notice: 'Your cart is currently empty' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class LineItemsController < ApplicationController
   # GET /line_items
   # GET /line_items.json
@@ -44,15 +52,16 @@ class LineItemsController < ApplicationController
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
 
-
     respond_to do |format|
       if @line_item.save
-	  
-        format.html { redirect_to @line_item.cart }
-        format.json { render json: @line_item, status: :created, location: @line_item }
+        format.html { redirect_to store_url }
+		format.js { @current_item = @line_item }
+        format.json { render json: @line_item,
+          status: :created, location: @line_item }
       else
         format.html { render action: "new" }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.json { render json: @line_item.errors,
+          status: :unprocessable_entity }
       end
     end
   end
